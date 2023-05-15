cc42f0d0ca685a921276bfbbce52b8ed87b19ae8
kayeedelau <125858321+kayeedelau@users.noreply.github.com>
2023-05-15 23:37:32 +0800

    Update tile.py

--- tile.py
+++ tile.py
@@ -4,6 +4,6 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,groups):
         super().__init__(groups)
-        self.image = pygame.image.load('../test.png').convert_alpha()
+        self.image = pygame.image.load('/home/kelvinyeh/group13_project/graphics/test.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         
