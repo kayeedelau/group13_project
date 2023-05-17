faabfc3fc131b4a40ed3615d5a6f2a6a33e56d5e
kayeedelau <125858321+kayeedelau@users.noreply.github.com>
2023-05-17 22:50:38 +0800

    Delete Adam_16x16.tsx

--- Adam_16x16.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Adam_16x16" tilewidth="16" tileheight="16" tilecount="336" columns="24">
- <image source="../Downloads/Modern_Interiors_Free_v2.2 (1)/Modern tiles_Free/Characters_free/Adam_16x16.png" width="384" height="224"/>
-</tileset>
