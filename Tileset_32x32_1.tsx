94a765018749a89dab39d540022b922ec189fdc4
kayeedelau <125858321+kayeedelau@users.noreply.github.com>
2023-05-17 22:51:08 +0800

    Delete Tileset_32x32_1.tsx

--- Tileset_32x32_1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Tileset_32x32_1" tilewidth="16" tileheight="16" tilecount="400" columns="20">
- <image source="../Downloads/Modern_Interiors_Free_v2.2 (1)/Modern tiles_Free/Old/Tileset_32x32_1.png" width="320" height="320"/>
-</tileset>
