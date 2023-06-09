5657f53fd5cd32b504b2e253a8f43dfd9e9d9a8b
KayeeDelau <kjijijoko@gmail.com>
2023-06-09 22:03:37 +0800

    0609 upload

--- main.py
+++ main.py
@@ -1,203 +1,96 @@
-import pygame
-import sys
-import json
+import pygame, sys, button
 from settings import *
 from level import Level
 
-class GameSave:
-	def __init__(self, player_id):
-		self.player_id = player_id
-		self.filename = f"{player_id}.json"
+class Game:
+	def __init__(self):
+
+		# general setup
+		pygame.init()
+		self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+		pygame.display.set_caption('Dream Adventure')
 		
-	def save_game(self, skin , health, mana, position):
-		game_data ={
-			"player_id": self.player_id,
-			"skin": skin,
-			"health": health,
-			"mana": mana,
-			"position": position
-		}
-		with open(self.filename, 'w') as file:
-			json.dump(game_data, file)
-	def load_game(self):
-		with open(self.filename, "r") as file:
-			game_data = json.load(file)
-		return game_data["skin"],game_data["health"],game_data["mana"],game_data["position"]
-
-class Button:
-	def __init__(self, text, font, x, y, width, height, color1, color2):
-		self.text = text
-		self.font = font
-		self.rect = pygame.Rect(x, y, width, height)
-		self.color1 = color1
-		self.color2 = color2
-
-	def draw(self, screen):
-		pygame.draw.rect(screen, self.color1, self.rect)
-		button_text_render = self.font.render(self.text, True, self.color2)
-		button_text_x = self.rect.x + (self.rect.width - button_text_render.get_width()) // 2
-		button_text_y = self.rect.y + (self.rect.height - button_text_render.get_height()) // 2
-		screen.blit(button_text_render, (button_text_x, button_text_y))
-
-	def is_clicked(self, mouse_pos):
-		return self.rect.collidepoint(mouse_pos)
-
-
-class Lobby:
-	def __init__(self, screen):
-		self.screen = screen
-		self.buttons = []
-		self.create_buttons()
-
-	def create_buttons(self):
-		button_font = pygame.font.SysFont(None, 90)
-		button_text = ["START", "LOAD", "QUIT"]
-		button_y = HEIGHT // 2
-
-		for text in button_text:
-			button_render = button_font.render(text, True, color2)
-			button_width, button_height = 200, 50
-			button_x = (WIDTH - button_width) // 2
-			button_rect = pygame.Rect(button_x, button_y, button_width, button_height)
-			button = Button(text, button_font, button_x, button_y, button_width, button_height, color1, color2)
-			self.buttons.append(button)
-			button_y += button_height + 20
-
-	def handle_events(self):
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				sys.exit()
-			elif event.type == pygame.MOUSEBUTTONDOWN:
-				mouse_pos = pygame.mouse.get_pos()
-				return self.handle_lobby_click(mouse_pos)
-
-	def handle_lobby_click(self, mouse_pos):
-		for button in self.buttons:
-			if button.is_clicked(mouse_pos):
-				if button.text == "START":
-					return "start"
-				elif button.text == "LOAD":
-					return self.handle_load_button()
-				elif button.text == "QUIT":
-					return "quit"
-					
-	def handle_load_button(self):
-		pygame.draw.rect(self.screen, BLACK, (0,0,WIDTH, HEIGHT))
-		font = pygame.font.SysFont(None, 60)
-		text_surface = font.render("Enter Player ID:", True, WHITE)
-		text_rect = text_surface.get_rect(center=(WIDTH //2, HEIGHT//2))
-		input_rect = pygame.Rect(text_rect.x, text_rect.y + 100, text_rect.width, 40)
-		input_active = True
-		player_id = ""
+		self.clock = pygame.time.Clock()
+		self.level = Level()
 		
-		while input_active:
+		
+	def draw_text(text, font, text_col, x, y):
+		img = font.render(text,True, text_col)
+		screen.blit(img, (x,y))
+	
+	
+	def run(self):
+		run = True
+		# menu setting
+		game_paused = False
+		menu_state= 'main'
+		
+		font = pygame.font.SysFont('arialblack',40)
+		TEXT_COL = (255,255,255)
+		
+		# image loading
+		resume_img  = pygame.image.load('./graphics/resume.png').convert_alpha()
+		options_img = pygame.image.load('./graphics/option.png').convert_alpha()
+		quit_img    = pygame.image.load('./graphics/quit.png').convert_alpha()
+		load_img    = pygame.image.load('./graphics/load.png').convert_alpha()
+		audio_img   = pygame.image.load('./graphics/audio.png').convert_alpha()
+		audio_gray  = pygame.image.load('./graphics/audio_gray.png').convert_alpha()
+		start_img   = pygame.image.load('./graphics/start.png').convert_alpha()
+		back_img    = pygame.image.load('./graphics/back.png').convert_alpha()
+		
+		#create button instances
+		resume_button  = button.Button(WIDTH//2, HEIGHT//7*1.5, resume_img, 1)
+		option_button  = button.Button(WIDTH//2, HEIGHT//7*3, options_img, 1)
+		quit_button    = button.Button(WIDTH//2, HEIGHT//7*4.5, quit_img, 1)
+		load_button    = button.Button(WIDTH//2, HEIGHT//7, load_img, 1)
+		audio_normal   = button.Button(WIDTH//2, HEIGHT//7*3, audio_img, 1)
+		audio_gray     = button.Button(WIDTH//2, HEIGHT//7*3, audio_gray, 1)
+		start_button   = button.Button(WIDTH//2, 325, start_img, 1)
+		back_button    = button.Button(WIDTH//2, HEIGHT//7*4.5, back_img, 1)
+		
+		# sound 
+		main_sound = pygame.mixer.Sound('./audio/main.ogg')
+		main_sound.set_volume(0.5)
+		main_sound.play(loops = -1)
+		# pause control
+		self.is_time_stopped = False
+		
+		# audio control
+		self.is_mute = False
+		
+		while run:
+			if not self.is_mute:
+				main_sound.set_volume(0.5)
+				audio_button = audio_normal
+			else:
+				main_sound.set_volume(0)
+				audio_button = audio_gray
+				
+			self.screen.fill(BLACK)
+			
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					pygame.quit()
 					sys.exit()
-				elif event.type == pygame.KEYDOWN:
-					if event.key == pygame.K_RETURN:
-						input_active = False
-					elif event.key == pygame.K_BACKSPACE:
-						player_id = player_id[:-1]
-					else:
-						player_id += event.unicode
-
-			pygame.draw.rect(self.screen, BLACK, (0, 0, WIDTH, HEIGHT))
-			pygame.draw.rect(self.screen, WHITE, input_rect, 2)
-			self.screen.blit(text_surface, text_rect)
-			font_input = pygame.font.SysFont(None, 40)
-			input_surface = font_input.render(player_id, True, WHITE)
-			self.screen.blit(input_surface, (input_rect.x + 5, input_rect.y + 5))
-			input_rect.w = max(200, input_surface.get_width() + 10)
-			pygame.display.flip()
-
-		return "load", player_id
-
-		
-	def draw(self):
-		self.screen.fill(BLACK)
-		self.draw_title()
-		for button in self.buttons:
-			button.draw(self.screen)
-		pygame.display.update()
-
-	def draw_title(self):
-		title_font = pygame.font.SysFont(None, 100)
-		title_text = "DREAM ADVENTURE"
-		title_render = title_font.render(title_text, True, WHITE)
-		title_x = (WIDTH - title_render.get_width()) // 2
-		title_y = 100
-		self.screen.blit(title_render, (title_x, title_y))
-
-
-class Game:
-	def __init__(self):
-		pygame.init()
-		self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-		pygame.display.set_caption('Dream Adventure')
-		self.clock = pygame.time.Clock()
-		self.level = Level()
-		self.in_lobby = True
-		self.lobby = Lobby(self.screen)
-		self.game_over = False
-		self.game_over_font = pygame.font.SysFont(None, 100)
-		
-		# sound
-		main_sound = pygame.mixer.Sound('./audio/main.ogg')
-		main_sound.set_volume(0.5)
-		main_sound.play(loops = -1)
-
-	def run(self):
-		while True:
-			if self.in_lobby:
-				action = self.handle_lobby()
-				if action == "start":
-					self.in_lobby = False
-				elif action == "quit":
-					pygame.quit()
-					sys.exit()
-			else:
-				for event in pygame.event.get():
-					if event.type == pygame.QUIT:
-						pygame.quit()
-						sys.exit()
-					if event.type == pygame.KEYDOWN:
-						if event.key == pygame.K_m:self.level.toggle_menu()
-				self.screen.fill(BLACK)
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_p:
+						game_paused = True
+			
+			if game_paused:
+				self.is_time_stopped = True
+				if menu_state == "main":
+					if resume_button.draw(self.screen):
+						game_paused = False
+						self.is_time_stopped = False
+					if audio_button.draw(self.screen):
+						self.is_mute = not self.is_mute
+					if quit_button.draw(self.screen):
+						run = False
+			if not self.is_time_stopped:
 				self.level.run()
-				pygame.display.update()
-				self.clock.tick(FPS)
-
-	def display_game_over(self):
-		game_over_text = self.game_over_font.render("Game Over", True, RED)
-		game_over_rect = game_over_text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-		self.screen.blit(game_over_text, game_over_rect)
-		pygame.display.update()
-		
-	def handle_player_death(self):
-		self.display_game_over()
-		pygame.time.delay(3000)  # Pause for 3 seconds before quitting the game
-		pygame.quit()
-		sys.exit()
-	def handle_player_update(self):
-		# ... Existing code for handling player update ...
-
-		if self.level.player.is_dead(): 
-			self.game_over = True
-			self.handle_player_death()	
-	def handle_lobby(self):
-		if self.game_over:
-			return
-		while self.in_lobby:
-			self.lobby.draw()
-			action = self.lobby.handle_events()
-			if action:
-				return action
-
-
+			pygame.display.update()
+			self.clock.tick(FPS)
+			
 if __name__ == '__main__':
 	game = Game()
 	game.run()
-
