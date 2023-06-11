ccd3ca9daa9a3cb4d428bf880a2871344ada8818
KayeeDelau <kjijijoko@gmail.com>
2023-06-11 22:18:58 +0800

    clean code

--- support.py
+++ support.py
@@ -18,5 +18,4 @@ def import_folder(path):
 			full_path = path + '/' + image
 			image_surf = pygame.image.load(full_path).convert_alpha()
 			surface_list.append(image_surf)
-			
 	return surface_list
